4ee0552df59f188856002087e2af6bd27cf770a4
Konstantin Bukin <bukin@inetstudio.ru>
2018-08-15 16:09:50 +0300

    Change bindings provider

--- src/Providers/BattlesBindingsServiceProvider.php
+++ src/Providers/BattlesBindingsServiceProvider.php
@@ -3,33 +3,55 @@
 namespace InetStudio\Battles\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class BattlesBindingsServiceProvider.
  */
 class BattlesBindingsServiceProvider extends ServiceProvider
 {
+    /**
+    * @var  bool
+    */
     protected $defer = true;
 
-    public $bindings = [];
-
     /**
-     * BattlesBindingsServiceProvider constructor.
-     *
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-
-        $this->bindings = getPackageBindings(__DIR__.'/../Contracts');
-    }
+    * @var  array
+    */
+    public $bindings = [
+        'InetStudio\Battles\Contracts\Events\Back\ModifyBattleEventContract' => 'InetStudio\Battles\Events\Back\ModifyBattleEvent',
+        'InetStudio\Battles\Contracts\Http\Controllers\Back\BattlesControllerContract' => 'InetStudio\Battles\Http\Controllers\Back\BattlesController',
+        'InetStudio\Battles\Contracts\Http\Controllers\Back\BattlesDataControllerContract' => 'InetStudio\Battles\Http\Controllers\Back\BattlesDataController',
+        'InetStudio\Battles\Contracts\Http\Controllers\Back\BattlesUtilityControllerContract' => 'InetStudio\Battles\Http\Controllers\Back\BattlesUtilityController',
+        'InetStudio\Battles\Contracts\Http\Controllers\Front\BattlesVotesControllerContract' => 'InetStudio\Battles\Http\Controllers\Front\BattlesVotesController',
+        'InetStudio\Battles\Contracts\Http\Requests\Back\SaveBattleRequestContract' => 'InetStudio\Battles\Http\Requests\Back\SaveBattleRequest',
+        'InetStudio\Battles\Contracts\Http\Responses\Back\Battles\DestroyResponseContract' => 'InetStudio\Battles\Http\Responses\Back\Battles\DestroyResponse',
+        'InetStudio\Battles\Contracts\Http\Responses\Back\Battles\FormResponseContract' => 'InetStudio\Battles\Http\Responses\Back\Battles\FormResponse',
+        'InetStudio\Battles\Contracts\Http\Responses\Back\Battles\IndexResponseContract' => 'InetStudio\Battles\Http\Responses\Back\Battles\IndexResponse',
+        'InetStudio\Battles\Contracts\Http\Responses\Back\Battles\SaveResponseContract' => 'InetStudio\Battles\Http\Responses\Back\Battles\SaveResponse',
+        'InetStudio\Battles\Contracts\Http\Responses\Back\Battles\ShowResponseContract' => 'InetStudio\Battles\Http\Responses\Back\Battles\ShowResponse',
+        'InetStudio\Battles\Contracts\Http\Responses\Back\Utility\SlugResponseContract' => 'InetStudio\Battles\Http\Responses\Back\Utility\SlugResponse',
+        'InetStudio\Battles\Contracts\Http\Responses\Back\Utility\SuggestionsResponseContract' => 'InetStudio\Battles\Http\Responses\Back\Utility\SuggestionsResponse',
+        'InetStudio\Battles\Contracts\Http\Responses\Front\BattlesVotes\VoteResponseContract' => 'InetStudio\Battles\Http\Responses\Front\BattlesVotes\VoteResponse',
+        'InetStudio\Battles\Contracts\Models\BattleModelContract' => 'InetStudio\Battles\Models\BattleModel',
+        'InetStudio\Battles\Contracts\Models\BattleVoteModelContract' => 'InetStudio\Battles\Models\BattleVoteModel',
+        'InetStudio\Battles\Contracts\Observers\BattleObserverContract' => 'InetStudio\Battles\Observers\BattleObserver',
+        'InetStudio\Battles\Contracts\Repositories\BattlesRepositoryContract' => 'InetStudio\Battles\Repositories\BattlesRepository',
+        'InetStudio\Battles\Contracts\Repositories\BattlesVotesRepositoryContract' => 'InetStudio\Battles\Repositories\BattlesVotesRepository',
+        'InetStudio\Battles\Contracts\Services\Back\BattlesDataTableServiceContract' => 'InetStudio\Battles\Services\Back\BattlesDataTableService',
+        'InetStudio\Battles\Contracts\Services\Back\BattlesObserverServiceContract' => 'InetStudio\Battles\Services\Back\BattlesObserverService',
+        'InetStudio\Battles\Contracts\Services\Back\BattlesServiceContract' => 'InetStudio\Battles\Services\Back\BattlesService',
+        'InetStudio\Battles\Contracts\Services\Front\BattlesServiceContract' => 'InetStudio\Battles\Services\Front\BattlesService',
+        'InetStudio\Battles\Contracts\Services\Front\BattlesVotesServiceContract' => 'InetStudio\Battles\Services\Front\BattlesVotesService',
+        'InetStudio\Battles\Contracts\Transformers\Back\BattleTransformerContract' => 'InetStudio\Battles\Transformers\Back\BattleTransformer',
+        'InetStudio\Battles\Contracts\Transformers\Back\SuggestionTransformerContract' => 'InetStudio\Battles\Transformers\Back\SuggestionTransformer',
+        'InetStudio\Battles\Contracts\Transformers\Front\BattlesFeedItemsTransformerContract' => 'InetStudio\Battles\Transformers\Front\BattlesFeedItemsTransformer',
+        'InetStudio\Battles\Contracts\Transformers\Front\BattlesSiteMapTransformerContract' => 'InetStudio\Battles\Transformers\Front\BattlesSiteMapTransformer',
+    ];
 
     /**
      * Получить сервисы от провайдера.
      *
-     * @return array
+     * @return  array
      */
     public function provides()
     {
